99e20ab337cd451b21f5a8553c3167955ff1ddfc
eli <46949964+securityELI@users.noreply.github.com>
2020-03-11 10:12:36 -0500

    Create RF_RiskScoreGrabber.py

---
+++ RF_RiskScoreGrabber.py
@@ -0,0 +1,41 @@
+import urllib.request
+import json
+
+f = open('very_critical.txt','w')
+f = open('critical.txt','w')
+f = open('high.txt','w')
+f = open('medium.txt','w')
+f = open('low.txt','w')
+f = open('informational.txt','w')
+
+token = '' #INSERT YOUR TOKEN HERE
+CVE_Collection = [line.rstrip('\n') for line in open('cve')]
+length = len(CVE_Collection)
+
+for i in range (0, length):
+    cve = CVE_Collection[i]
+    url = "https://api.recordedfuture.com/v2/vulnerability/"+cve+"?fields=risk&metadata=false"
+    req = urllib.request.Request(url, None, {'X-RFToken': token})   
+    with urllib.request.urlopen(req) as res:
+        raw = (res.read())
+        parse = json.loads(raw)
+        score = int((parse['data']['risk']['score']))
+                   
+        if (score >= 90):
+            with open('very_critical.txt','a') as dfile:
+                dfile.write(cve +',')
+        elif (80<=score<=89):
+            with open('critical.txt','a') as zfile:
+                zfile.write(cve +',')
+        elif (65<=score<= 79):   
+            with open('high.txt','a') as afile:
+                afile.write(cve + ',')
+        elif (25<=score<=64):
+                        with open('medium.txt','a') as bfile:
+                                bfile.write(cve + ',')
+        elif (5<=score<=24):
+                        with open('low.txt','a') as cfile:
+                                cfile.write(cve + ',')
+        else:
+                        with open('informational.txt','a') as afile:
+                                afile.write(cve + ',')
